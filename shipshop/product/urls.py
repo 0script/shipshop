9e786b26666aa6eb4dba499c5dce42d9374f093e
0script <z5r00script@gmail.com>
2023-04-06 07:09:07 -0400

    view product detail ok

--- shipshop/product/urls.py
+++ shipshop/product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import *
 
-
 urlpatterns=[
     path('latest-products/',LatestProductsView.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>',ProductDetail.as_view()),
 ]
