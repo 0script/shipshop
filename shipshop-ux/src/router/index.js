cc0b74d9859615a79edf6b1d5a52b29d9aa3c91c
0script <z5r00script@gmail.com>
2023-04-08 11:21:07 -0400

    sign up view done

--- shipshop-ux/src/router/index.js
+++ shipshop-ux/src/router/index.js
@@ -5,6 +5,7 @@ import ProductView from '../views/ProductView.vue'
 import CategoryView from '../views/CategoryView.vue'
 import SearchView from '../views/SearchView.vue'
 import CartView from '../views/CartView.vue'
+import SignUpView from '../views/SignUpView.vue'
 
 const routes = [
   {
@@ -19,6 +20,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'sign-up',
+    component: SignUpView,
+    //props: route => ({ query: route.query.q })
   },
   {
     path: '/search',
---
+++ shipshop-ux/src/views/SignUpView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="page-sign-up">
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <h1 class="title">Sign up</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios'
+    import { toast } from 'bulma-toast'
+
+
+    export default{
+        name:'SignUpView',
+        data(){
+            return{
+                username:'',
+                password:'',
+                password2:'',
+                errors:[]
+            }
+        }
+
+    }
+</script>
