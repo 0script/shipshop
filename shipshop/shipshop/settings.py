5e42c82eb8a5630e2e9a0d7701d2cddbdd5d5120
0script <z5r00script@gmail.com>
2023-04-11 07:49:42 -0400

    checkout in proc

--- shipshop/shipshop/settings.py
+++ shipshop/shipshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
     'product',
+    'order',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -86,13 +87,22 @@ WSGI_APPLICATION = 'shipshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'shipshopdubai',
+        'PASSWORD': 'x4kruMGSF3Qv',
+        'HOST': 'ep-jolly-frost-286375.us-east-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
